6793fda1c1b8821dee696efaab721324e36c7fdb
Juan Marchese <juanimarchese@gmail.com>
2014-06-22 03:06:31 -0300

    Modelo de dominio inicial

--- pcrh/test/unit/com/pcrh/HechoSpec.groovy
+++ pcrh/test/unit/com/pcrh/HechoSpec.groovy
@@ -1,13 +1,14 @@
-package com
+package com.pcrh
 
+import com.pcrh.Hecho
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(Pcrh)
-class PcrhSpec extends Specification {
+@TestFor(Hecho)
+class HechoSpec extends Specification {
 
     def setup() {
     }
